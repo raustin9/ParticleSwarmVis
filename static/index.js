9b26d7f2bc656b786bccc50dafd22a7911af19e2
raustin9 <raustin9@vols.utk.edu>
2024-04-08 16:08:34 -0400

    grid framework in place

--- static/index.js
+++ static/index.js
@@ -1,9 +1,37 @@
 import { Swarm } from "./swarm.js";
 
 class App {
-    constructor() {
+    constructor(dimension, numx, numy, boxw, boxh) {
         this.swarm = new Swarm();
+        this.grid = document.getElementById("swarm-grid");
+        this.context = this.grid.getContext("2d");
+        this.context.canvas.width = dimension;
+        this.context.canvas.height = dimension;
+
+        this.grid_data = Array(numx)
+            .fill()
+            .map(() => Array(numy).fill(0));
+        
+        this.boxw = boxw;
+        this.boxh = boxh;
+    }
+
+    create_grid() {
+        for (let i = 0; i < this.grid_data.length; i++) {
+            for (let j = 0; j <= this.grid_data.length; j++) {
+                this.context.beginPath();
+                this.context.rect(
+                    i * this.boxw,
+                    j * this.boxh,
+                    this.boxw,
+                    this.boxh
+                );
+                this.context.strokeStyle = "green";
+                this.context.stroke();
+            }
+        }
     }
 }
 
-let app = new App();
+let app = new App(800, 80, 80, 15, 15);
+app.create_grid();
