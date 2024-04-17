a55bbfe87d91d304aa81cdd0c3f30f2d1835ba9b
Jacob Hawkins <jacob.hawkins010@gmail.com>
2024-04-17 11:01:43 -0400

    reset back to grid

--- static/index.js
+++ static/index.js
@@ -1,28 +1,28 @@
-import { Swarm } from "./swarm.js";
-import Renderer from "./renderer/renderer.js";
+import { Swarm } from './swarm.js';
+import Renderer from './renderer/renderer.js';
 
 class App {
     constructor(numx, numy, box_size) {
         this.swarm = new Swarm();
-        
-        this.grid = document.getElementById("swarm-grid");
-        // this.context = this.grid.getContext("2d");
-        // this.context.canvas.width = numx * box_size;
-        // this.context.canvas.height = numy * box_size;
 
-        this.renderer = new Renderer(this.grid, 640, 480);
+        this.grid = document.getElementById('swarm-grid');
+        this.context = this.grid.getContext('2d');
+        this.context.canvas.width = numx * box_size;
+        this.context.canvas.height = numy * box_size;
 
-//        this.grid_data = Array(numx)
-//            .fill()
-//            .map(() => Array(numy).fill(0));
-//
-//        for (let i = 0; i < this.grid_data.length; i++) {
-//            for (let j = 0; j <= this.grid_data.length; j++) {
-//                this.grid_data[i][j] = Math.floor(Math.random() * 4);
-//            }
-//        }
-        
-//        this.box_size = box_size;
+        // this.renderer = new Renderer(this.grid, 640, 480);
+
+        this.grid_data = Array(numx)
+            .fill()
+            .map(() => Array(numy).fill(0));
+
+        for (let i = 0; i < this.grid_data.length; i++) {
+            for (let j = 0; j <= this.grid_data.length; j++) {
+                this.grid_data[i][j] = Math.floor(Math.random() * 4);
+            }
+        }
+
+        this.box_size = box_size;
     }
 
     // Create 2D grid on canvas
@@ -36,8 +36,8 @@ class App {
                     this.box_size,
                     this.box_size
                 );
-                this.context.strokeStyle = "#363636";
-                this.context.fillStyle = "cyan";
+                this.context.strokeStyle = '#363636';
+                this.context.fillStyle = 'cyan';
                 if (this.grid_data[i][j] === 3) {
                     this.context.fill();
                 } else {
@@ -54,5 +54,5 @@ class App {
 }
 
 let app = new App(130, 70, 10, 10);
-// app.create_grid();
-app.create_webgl();
+app.create_grid();
+// app.create_webgl();
